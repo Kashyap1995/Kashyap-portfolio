5879d9155eecaec57130a2eac1434b69e241c4db
Kashyap1995 <kkdave1995@gmail.com>
2026-04-14 20:24:44 +0530

    Updated versal file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'  // 🔥 CHANGE HERE
 import { motion } from 'framer-motion'
 
 import Navigation from './components/Navigation'
